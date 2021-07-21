b9c7a2f0bf78f9c238451048cdba25261e683275
粘婷婷 ting <ting885522@gmail.com>
2021-07-21 17:06:00 +0800

    Project Init

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace WindowsFormsApp2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblYear = new System.Windows.Forms.RadioButton();
+			this.lblMonth = new System.Windows.Forms.RadioButton();
+			this.lblCar = new System.Windows.Forms.ComboBox();
+			this.lblMotor = new System.Windows.Forms.ComboBox();
+			this.lblResult = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.dtpChoose2 = new System.Windows.Forms.DateTimePicker();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.dtpChoose = new System.Windows.Forms.DateTimePicker();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.GroupBoxresult = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnSubmit = new System.Windows.Forms.Button();
+			this.btnCancle = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.GroupBoxresult.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lblYear
+			// 
+			this.lblYear.AutoSize = true;
+			this.lblYear.Location = new System.Drawing.Point(18, 39);
+			this.lblYear.Name = "lblYear";
+			this.lblYear.Size = new System.Drawing.Size(76, 19);
+			this.lblYear.TabIndex = 1;
+			this.lblYear.Text = "全年度";
+			this.lblYear.UseVisualStyleBackColor = true;
+			// 
+			// lblMonth
+			// 
+			this.lblMonth.AutoSize = true;
+			this.lblMonth.Location = new System.Drawing.Point(97, 39);
+			this.lblMonth.Name = "lblMonth";
+			this.lblMonth.Size = new System.Drawing.Size(76, 19);
+			this.lblMonth.TabIndex = 1;
+			this.lblMonth.Text = "依期間";
+			this.lblMonth.UseVisualStyleBackColor = true;
+			this.lblMonth.CheckedChanged += new System.EventHandler(this.lblMonth_CheckedChanged);
+			// 
+			// lblCar
+			// 
+			this.lblCar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.lblCar.FormattingEnabled = true;
+			this.lblCar.Items.AddRange(new object[] {
+            "機車",
+            "貨車",
+            "大客車",
+            "自用小客車",
+            "營業用小客車"});
+			this.lblCar.Location = new System.Drawing.Point(49, 34);
+			this.lblCar.Name = "lblCar";
+			this.lblCar.Size = new System.Drawing.Size(121, 23);
+			this.lblCar.TabIndex = 2;
+			// 
+			// lblMotor
+			// 
+			this.lblMotor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.lblMotor.FormattingEnabled = true;
+			this.lblMotor.Items.AddRange(new object[] {
+            "0-150cc",
+            "150cc~250cc",
+            "251cc~500cc",
+            "501cc~600cc",
+            "601cc~1200cc",
+            "1201cc~1800cc",
+            "1801cc~2400cc",
+            "2401cc~3000cc",
+            "3001cc~3600cc",
+            "3601cc~4200cc",
+            "4201cc~4800cc",
+            "4801cc~5400cc",
+            "5401cc~6000cc",
+            "6001cc~6600cc",
+            "6601cc~7200cc",
+            "7201cc~7800cc",
+            "7801cc~8400cc",
+            "8401cc~9000cc",
+            "9001cc~9600cc",
+            "9601cc~10200cc",
+            "10201cc以上"});
+			this.lblMotor.Location = new System.Drawing.Point(6, 34);
+			this.lblMotor.Name = "lblMotor";
+			this.lblMotor.Size = new System.Drawing.Size(252, 23);
+			this.lblMotor.TabIndex = 2;
+			this.lblMotor.SelectedIndexChanged += new System.EventHandler(this.lblMotor_SelectedIndexChanged);
+			// 
+			// lblResult
+			// 
+			this.lblResult.AutoSize = true;
+			this.lblResult.Location = new System.Drawing.Point(259, 39);
+			this.lblResult.Name = "lblResult";
+			this.lblResult.Size = new System.Drawing.Size(15, 15);
+			this.lblResult.TabIndex = 3;
+			this.lblResult.Text = "-";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.groupBox1.Controls.Add(this.dtpChoose2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.dtpChoose);
+			this.groupBox1.Controls.Add(this.lblYear);
+			this.groupBox1.Controls.Add(this.lblMonth);
+			this.groupBox1.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.groupBox1.Location = new System.Drawing.Point(201, 138);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(557, 100);
+			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "使用期間";
+			// 
+			// dtpChoose2
+			// 
+			this.dtpChoose2.Location = new System.Drawing.Point(404, 34);
+			this.dtpChoose2.Name = "dtpChoose2";
+			this.dtpChoose2.Size = new System.Drawing.Size(147, 25);
+			this.dtpChoose2.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(365, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(23, 15);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "至";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(176, 41);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(23, 15);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "從";
+			// 
+			// dtpChoose
+			// 
+			this.dtpChoose.Location = new System.Drawing.Point(204, 34);
+			this.dtpChoose.Name = "dtpChoose";
+			this.dtpChoose.Size = new System.Drawing.Size(146, 25);
+			this.dtpChoose.TabIndex = 5;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.Color.Azure;
+			this.groupBox2.Controls.Add(this.lblCar);
+			this.groupBox2.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.groupBox2.Location = new System.Drawing.Point(201, 244);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(276, 78);
+			this.groupBox2.TabIndex = 5;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "用途";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.BackColor = System.Drawing.Color.Azure;
+			this.groupBox3.Controls.Add(this.lblMotor);
+			this.groupBox3.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.groupBox3.Location = new System.Drawing.Point(483, 244);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(275, 78);
+			this.groupBox3.TabIndex = 6;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "汽缸CC數/馬達馬力";
+			// 
+			// GroupBoxresult
+			// 
+			this.GroupBoxresult.BackColor = System.Drawing.Color.White;
+			this.GroupBoxresult.Controls.Add(this.lblResult);
+			this.GroupBoxresult.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.GroupBoxresult.Location = new System.Drawing.Point(201, 328);
+			this.GroupBoxresult.Name = "GroupBoxresult";
+			this.GroupBoxresult.Size = new System.Drawing.Size(557, 120);
+			this.GroupBoxresult.TabIndex = 7;
+			this.GroupBoxresult.TabStop = false;
+			this.GroupBoxresult.Text = "試算結果";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("標楷體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label2.Location = new System.Drawing.Point(292, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(320, 28);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "使用牌照稅應納稅額試算";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.Location = new System.Drawing.Point(155, 72);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(647, 45);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "\r\n1.本表試算之稅額僅供參考之用，不做任何證明，實際應納稅額仍應以稽徵機關核定為準。\r\n2.使用牌照稅稅額對照表及電動車應納稅額表。\r\n";
+			// 
+			// btnSubmit
+			// 
+			this.btnSubmit.ForeColor = System.Drawing.Color.SteelBlue;
+			this.btnSubmit.Location = new System.Drawing.Point(340, 454);
+			this.btnSubmit.Name = "btnSubmit";
+			this.btnSubmit.Size = new System.Drawing.Size(83, 30);
+			this.btnSubmit.TabIndex = 10;
+			this.btnSubmit.Text = "確定送出";
+			this.btnSubmit.UseVisualStyleBackColor = true;
+			this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+			// 
+			// btnCancle
+			// 
+			this.btnCancle.ForeColor = System.Drawing.Color.SteelBlue;
+			this.btnCancle.Location = new System.Drawing.Point(483, 454);
+			this.btnCancle.Name = "btnCancle";
+			this.btnCancle.Size = new System.Drawing.Size(86, 30);
+			this.btnCancle.TabIndex = 10;
+			this.btnCancle.Text = "取消重填";
+			this.btnCancle.UseVisualStyleBackColor = true;
+			this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label4.ForeColor = System.Drawing.Color.Red;
+			this.label4.Location = new System.Drawing.Point(157, 68);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(95, 15);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "貼心小叮嚀:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(920, 516);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.btnCancle);
+			this.Controls.Add(this.btnSubmit);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.GroupBoxresult);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.GroupBoxresult.ResumeLayout(false);
+			this.GroupBoxresult.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.RadioButton lblYear;
+		private System.Windows.Forms.RadioButton lblMonth;
+		private System.Windows.Forms.ComboBox lblCar;
+		private System.Windows.Forms.ComboBox lblMotor;
+		private System.Windows.Forms.Label lblResult;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.DateTimePicker dtpChoose2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.DateTimePicker dtpChoose;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox GroupBoxresult;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnSubmit;
+		private System.Windows.Forms.Button btnCancle;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.Init();
+		}
+
+		private void Init()
+		{
+			this.lblYear.Checked = true;
+			this.lblCar.SelectedIndex = 0;
+			this.lblMotor.SelectedIndex = 0;
+			//清除groupboxresult.text的內容，回到原本的試算結果
+			this.GroupBoxresult.Text = "試算結果";		
+		}
+
+		private void btnCancle_Click(object sender, EventArgs e)
+		{
+			//執行void int的內容
+			this.Init();
+		}
+		
+		private void lblMotor_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//沒用不小心點到的
+		}
+
+		//==============================================================================
+		private int GetTaxPercentage()
+		{
+			//取得排氣量選單對應的價格
+			string taxType1 = this.lblMotor.Text;
+			switch (taxType1)
+			{
+				case "150cc以下":					
+					return 100;
+				case "151cc~250cc":					
+					return 800;
+				case "251cc~500cc":			
+					return 1620;
+				case "501cc~600cc":
+					return 2160;
+				case "601cc~1200cc":
+					return 4320;
+				case "1201cc~1800cc":
+					return 7120;
+				case "1801cc或以上":
+					return 11230;
+				default:
+					return 0;
+			}
+			
+
+		}
+		private int GetTaxPercentage2()
+		{
+			//取得排氣量選單對應的價格
+			string taxType1 = this.lblMotor.Text;
+			switch (taxType1)
+			{
+				case "500cc以下":
+					return 1620;
+				case "501cc~600cc":
+					return 2160;
+				case "601cc~1200cc":
+					return 4320;
+				case "1201cc - 1800cc":
+					return 7120;
+				case "1801cc - 2400cc":
+					return 11230;
+				case "2401cc - 3000cc":
+					return 15210;
+				case "3001cc - 4200cc":
+					return 28220;
+				case "4201cc - 5400cc":
+					return 46170;
+				case "5401cc - 6600cc":
+					return 69690;
+				case "6601cc - 7800cc":
+					return 117000;
+				case "7801cc 以上":
+					return 151200;
+				default:
+					return 0;
+			}
+
+
+		}
+		private int GetTaxPercentage3()
+		{
+			//取得排氣量選單對應的價格
+			string taxType1 = this.lblMotor.Text;
+			switch (taxType1)
+			{
+				case "500cc以下":
+					return 900;
+				case "501cc~600cc":
+					return 1260;
+				case "601cc~1200cc":
+					return 2160;
+				case "1201cc - 1800cc":
+					return 3060;
+				case "1801cc - 2400cc":
+					return 6480;
+				case "2401cc - 3000cc":
+					return 9900;
+				case "3001cc - 4200cc":
+					return 16380;
+				case "4201cc - 5400cc":
+					return 24300;
+				case "5401cc - 6600cc":
+					return 33660;
+				case "6601cc - 7800cc":
+					return 44460;
+				case "7801cc 以上":
+					return 56700;
+				default:
+					return 0;
+			}
+		}
+		private int GetTaxPercentage4()
+		{
+			//取得排氣量選單對應的價格
+			string taxType1 = this.lblMotor.Text;
+			switch (taxType1)
+			{
+				case "500cc以下":
+					return 0;
+				case "501cc~600cc":
+					return 1080;
+				case "601cc~1200cc":
+					return 1800;
+				case "1201cc - 1800cc":
+					return 2700;
+				case "1801cc - 2400cc":
+					return 3600;
+				case "2401cc - 3000cc":
+					return 4500;
+				case "3001cc - 3600cc":
+					return 5400;
+				case "3601cc - 4200cc":
+					return 6300;
+				case "4201cc - 4800cc":
+					return 7200;
+				case "4801cc - 5400cc":
+					return 8100;
+				case "5401cc - 6000cc":
+					return 9000;
+				case "6001cc - 6600cc":
+					return 9900;
+				case "6601cc~7200cc":
+					return 10800;
+				case "7201cc~7800cc":
+					return 11700;
+				case "7801cc~8400cc":
+					return 12600;
+				case "8401cc~9000cc":
+					return 13500;
+				case "9001cc~9600cc":
+					return 14400;
+				case "9601cc~10200cc":
+					return 15300;
+				case "10201cc以上":
+					return 16200;
+				default:
+					return 0;
+			}
+		}
+
+		private string GetTaxPercentageCAR()
+		{
+
+			//取得車種
+			string taxType2 = this.lblCar.Text;
+			switch (taxType2)
+			{
+				case "機車":
+					return "機車";
+				case "貨車":
+					return "貨車";
+				case "大客車":
+					return "大客車";
+				case "自用小客車":
+					return "自用小客車";
+				case "營業用小客車":
+					return "營業用小客車";
+				default:
+					return "CAR?";
+			}
+		}
+
+		private void btnSubmit_Click(object sender, EventArgs e)
+		{
+			GroupBoxresult.Text += Environment.NewLine;
+			//取得日期天數，把日期選單選好的日期丟到date1 /date2存起來，用nian儲存兩個日期之間的差距天數，把資訊透過textbox show出來
+			int nian ,result;
+			DateTime date2 = dtpChoose2.Value;
+			DateTime date1 = dtpChoose.Value;
+			nian = (date2 - date1).Days + 1;
+			GroupBoxresult.Text += "使用期間:" + dtpChoose.Value + "到" + dtpChoose2.Value  + Environment.NewLine;
+			GroupBoxresult.Text += "計算天數:" + nian + Environment.NewLine;
+			//取得排氣量對應的價格
+			string carnumber = (string)this.GetTaxPercentageCAR();
+			GroupBoxresult.Text += "用途:" + carnumber + Environment.NewLine;
+
+			//取得車種，(可優化的方案:選擇車種跳出對應的排氣量選單)
+			int ccnumber = (int)this.GetTaxPercentage();
+			GroupBoxresult.Text += "汽缸CC數:" + ccnumber + Environment.NewLine;
+
+			//result儲存需繳納金額
+			result = ccnumber * nian / 365;
+			//下面show出公式
+			GroupBoxresult.Text += "計算公式:" + ccnumber + "*" + nian + "/365="+ result + Environment.NewLine;
+			GroupBoxresult.Text += "應納稅額: 共" + result +"元" + Environment.NewLine;			
+		}
+
+		private void lblMonth_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
